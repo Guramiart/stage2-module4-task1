a994fd256a2764908a18cfc2928c3e251733d5cf
Artem Pasko <guramibox777@gmail.com>
2022-11-30 23:23:18 +0300

    implement thread safe singleton pattern

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,22 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    // Write your code here!
+
+    private static volatile ThreadSafeSingleton instance;
+    private static final Object mutex = new Object();
+
+    private ThreadSafeSingleton() {}
+
+    public static ThreadSafeSingleton getInstance() {
+        ThreadSafeSingleton localInstance = instance;
+        if(localInstance == null) {
+            synchronized (mutex) {
+                localInstance = instance;
+                if(localInstance == null) {
+                    instance = localInstance = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return localInstance;
+    }
 }
